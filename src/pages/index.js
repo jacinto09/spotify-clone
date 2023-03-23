a700ebc5bfa9ec11ceb7cb9eaa3651a61389c5d8
Jacinto Martinez <jacintomartinez0909@gmail.com>
2023-03-23 19:00:08 +0100

    no around you imports ❌

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import TopCharts from './TopCharts'
 import TopArtists from './TopArtists'
-import AroundYou from './AroundYou'
 import Discover from './Discover'
 import SongDetails from './SongDetails'
 import Search from './Search'
 import ArtistDetails from './ArtistDetails'
 
-export { TopCharts, TopArtists, Discover, AroundYou, SongDetails, Search, ArtistDetails }
+export { TopCharts, TopArtists, Discover, SongDetails, Search, ArtistDetails }
